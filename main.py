b717490f5ac71ec23b608e3f12d1682f76def3e5
Tajik Tungi <tajiktungi22@gmail.com>
2026-06-17 01:41:35 +0530

    changed

--- main.py
+++ main.py
@@ -9,3 +9,8 @@ app.include_router(user_router)
 @app.get("/")
 def root():
     return {"message": "library manager api is running"}
+import os
+if __name__ == "main":
+    import uvicorn
+    port = int(os.environ.get("PORT",8000))
+    uvicorn.run("main:app",host="0.0.0.0",port=port)
